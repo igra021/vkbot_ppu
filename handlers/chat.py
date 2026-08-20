977b2b6879651f507ca377716067dcb0d6782c6a
igra021 <igra021@yandex.ru>
2026-08-20 20:13:22 +0400

    отправка тетефона клиента админу

--- handlers/chat.py
+++ handlers/chat.py
@@ -6,6 +6,7 @@ from vkbottle.bot import BotLabeler, Message
 from llm.chat_gpt import chat_gpt
 from service.is_telephone import is_telephone
 from service.text_cleaner import clean_text
+from init.config import vk_admin
 from loguru import logger
 import json
 
@@ -13,10 +14,19 @@ chat_labeler = BotLabeler()
 chat_labeler.vbml_ignore_case = True
 
 
+async def send_to_admin(vk_admin, user_id, attachments, message):
+    await message.ctx_api.messages.send(
+    peer_id=vk_admin,
+    message=f"Вложения от пользователя {user_id}:\n{attachments}",
+    random_id=0
+    )
+
+
 @chat_labeler.message()
 async def chat(message: Message):
-    # проверка работы чатбота- сообщения доходят до бота
-    # print(f"🔥 ПОЛУЧЕНО СООБЩЕНИЕ от {message.from_id}: {message.text}")
+    video_link = ''
+    response = ''
+    attachments = []
 
     try:
         user_id = message.from_id  # ✅ ID пользователя
@@ -27,10 +37,15 @@ async def chat(message: Message):
             # очистка сообщения клиента от мусора
             new_message = clean_text(message.text)
             # проверка на номер телефона в сообщении клиента
-            new_message = is_telephone(new_message)
+            new_message, telephone = is_telephone(new_message)
+            if telephone.lstrip():
+                # отправить админу телефон
+                await send_to_admin(vk_admin, user_id, 'телефон: ' + telephone, message)
 
-            result = await chat_gpt(user_id, new_message)
-            # result — это строка JSON от LLM
+            # обращение к ЛЛМ
+            if new_message:
+                result = await chat_gpt(user_id, new_message)
+            
             if result:
                 try:
                     data = json.loads(result)
@@ -44,16 +59,11 @@ async def chat(message: Message):
         else:
             response = ''
 
-        # Если ответ пустой — дефолтное сообщение
-        if not response or not response.strip():
-            response = "Извините, я не смог обработать ваш запрос. Попробуйте переформулировать вопрос."
-            logger.warning(f"⚠️ Пустой ответ для user_id={user_id}")
         
         # Обработка вложений (фото, видео)
         photos = [att.photo for att in message.attachments if att.photo]
         videos = [att.video for att in message.attachments if att.video]
         
-        attachments = []
         for photo in photos:
             attachments.append(f"photo{photo.owner_id}_{photo.id}")
         for video in videos:
@@ -61,7 +71,10 @@ async def chat(message: Message):
         
         if attachments:
             response += "\n\n✅ Ваши фото и видео пересланы администратору."
-            # Здесь код пересылки администратору
+
+            # ✅ Отправка сообщения администратору, ошибка - в виде списка, а не в виде объектов
+            await send_to_admin(vk_admin, user_id, attachments, message)
+
         
         # ✅ Отправляем сообщение в бот
         await message.answer(response)
@@ -71,6 +84,10 @@ async def chat(message: Message):
         if video_link:    
             await message.answer("Посмотрите видео по вашей ситуации: ", attachment=video_link)
         
+        # Если очищенное сообщение пустое
+        if not attachments and not new_message:
+            response = "Пустое сообщение. Попробуйте переформулировать вопрос."
+
     except Exception as e:
         logger.error(f"❌ Ошибка в обработчике: {e}")
         await message.answer("Извините, произошла ошибка. Попробуйте позже.")
--- service/is_telephone.py
+++ service/is_telephone.py
@@ -4,23 +4,24 @@ import phonenumbers
 from phonenumbers import PhoneNumberMatcher
 
 def is_telephone(text):
-
     """
     Заменяет все найденные номера на один статичный фальшивый
     """
+
     fake_number="+79120000000"
     # Собираем все найденные номера с их позициями
     matches = list(PhoneNumberMatcher(text, "RU"))
 
     if not matches:
-        return text
+        return text, ''
     
     # Заменяем с конца, чтобы не сбивать индексы
     result = text
+    telephone = ''
     for match in reversed(matches):
-        print(match.raw_string)
+        telephone = telephone + ' ' + match.raw_string
         start = match.start
         end = match.end
         result = result[:start] + fake_number + result[end:]
     
-    return result
+    return result, telephone
